cdbcf1ce4a6f1a3e5abefdfc6529d491bdb6fd27
ziljnk <linhnd2805@gmail.com>
2026-09-21 10:26:14 +0700

    Add new hostname to image domains in next.config.ts: Included 'test.linhouse.com.vn' to the list of allowed image sources for enhanced image serving capabilities.

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "test.linhouse.com.vn",
+      },
     ],
   },
 };
